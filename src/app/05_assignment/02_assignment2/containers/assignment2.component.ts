9da6894fc5d210b9c9723f0762a72f8eea8f6645
Amartya Parijat <amartya.parijat@nagarro.com>
2020-04-21 02:26:47 +0530

    Fixed Bugs

--- src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
+++ src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
@@ -1,35 +1,42 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { AssignmentComponent } from '../../assignment.component';
+import { AssignmentComponent, Todo } from '../../assignment.component';
 
 @Component({
   selector: 'app-assignment2',
   templateUrl: './assignment2.component.html',
   styleUrls: ['./assignment2.component.css'],
-  encapsulation: ViewEncapsulation.None
+  //encapsulation: ViewEncapsulation.None
 })
 export class Assignment2Component {
 
+  list : Todo[];
   constructor(private assgComp : AssignmentComponent) { }
 
   ngOnInit(){
-     var table = document.getElementById("tab")! as HTMLTableElement;
-     this.addData(table);
+     //var table = document.getElementById("tab")! as HTMLTableElement;
+     //this.addData(table);
+     this.list = this.assgComp.todos;
+    //  console.log(this.list);
   }
 
-  addData(table : HTMLTableElement){
-    var data = this.assgComp.todos;
-    for(let todo of data){
-      let row : HTMLTableRowElement = document.createElement('tr');
-      table.appendChild(row);
-      row.onclick = function() {
-        row.className = 'active';
-      }
-      row.appendChild(this.createTableData(todo.id));
-      row.appendChild(this.createTableData(todo.type));
-      row.appendChild(this.createTableData(todo.description));
-      row.appendChild(this.createTableData(String(todo.completed)));
-    }
+  addClass(event){
+    event.target.parentNode.className = "active";
   }
+
+  // addData(table : HTMLTableElement){
+  //   var data = this.assgComp.todos;
+  //   for(let todo of data){
+  //     let row : HTMLTableRowElement = document.createElement('tr');
+  //     table.appendChild(row);
+  //     row.onclick = function() {
+  //       row.className = 'active';
+  //     }
+  //     row.appendChild(this.createTableData(todo.id));
+  //     row.appendChild(this.createTableData(todo.type));
+  //     row.appendChild(this.createTableData(todo.description));
+  //     row.appendChild(this.createTableData(String(todo.completed)));
+  //   }
+  // }
 
   createTableData(data : string) : HTMLTableDataCellElement{
     let cell = document.createElement('td');
